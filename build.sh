4fcc84426fab2b9fe0b110067e571c5b9be4edb9
Radostin Stoyanov <radostin@redhat.com>
2022-07-28 10:07:56 +0100

    build: drop --storage-opt
    Docker fails with the following error:
    
    Error response from daemon: --storage-opt is supported only for overlay over xfs with 'pquota' mount option
    
    Signed-off-by: Radostin Stoyanov <radostin@redhat.com>

--- build.sh
+++ build.sh
@@ -15,7 +15,6 @@ mkdir -p build/
 for target in pi bmv2 p4c; do
     $CONTAINER_ENGINE run \
         --rm \
-        --storage-opt size=25G \
         --mount type=bind,source="$(pwd)/build",target=/mnt \
         p4lang-packages \
         bash -c "make ${target}-sdeb && dpkg -c *.deb && mv *.build *.buildinfo *.changes *.deb *.debian.tar.xz *.dsc *.orig.tar.gz /mnt";
